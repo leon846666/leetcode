94c2185d67dbf05fd47a26a37e9291264a54ea26
leon846666 <leon846666@gmail.com>
2018-05-24 09:08:53 -0400

    solution of question 595 big Countries

---
+++ src/leetcode/solution/BigCountries.java
@@ -0,0 +1,10 @@
+package leetcode.solution;
+
+public class BigCountries {
+	
+	public static void main(String[] args) {
+		
+		String sql ="select name ,population, area from World where population>25000000 or area >3000000";
+		
+	}
+}
