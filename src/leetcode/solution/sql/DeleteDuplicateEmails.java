af6d57cbf6a454d7f3314c5a0c419917296291c5
leon846666 <leon846666@gmail.com>
2018-06-06 17:00:45 -0400

    self link

---
+++ src/leetcode/solution/sql/DeleteDuplicateEmails.java
@@ -0,0 +1,14 @@
+package leetcode.solution.sql;
+
+public class DeleteDuplicateEmails {
+
+	/**
+	 *  Question :196. Delete Duplicate Emails
+	 *  Category: 	SQL    
+	 * 	Level : 	Easy
+	 * 	Result: 	Accepted
+	 * 	Author: 	Yang Liu
+	 * */
+	
+	String sql = "delete p from Person p ,Person p2 where p.Email=p2.Email and p2.Id <p.Id";
+}
