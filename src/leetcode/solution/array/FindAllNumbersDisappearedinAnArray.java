a4ad3806fd42af32f8cd735d7faef46b63df429b
leon846666 <leon846666@gmail.com>
2018-06-10 11:44:10 -0400

    using stream and lambda

---
+++ src/leetcode/solution/array/FindAllNumbersDisappearedinAnArray.java
@@ -0,0 +1,37 @@
+package leetcode.solution.array;
+
+import java.awt.event.MouseWheelEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FindAllNumbersDisappearedinAnArray {
+
+	public static List<Integer> findAllNumbersDisappearedinAnArray (int[] nums) {
+		List<Integer> list=IntStream.of(nums).boxed().collect(Collectors.toList());
+		Map<Integer,List<Integer>>map=list.stream().collect(Collectors.groupingBy(u -> u.intValue()));
+        List<Integer> num=new ArrayList();
+       for(int i=1;i<=nums.length;i++){
+           if(map.get(i)==null){
+              num.add(i); 
+           }
+       }
+       return num;
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		int[] nums={4,3,2,7,8,2,3,1};
+		
+		List<Integer> list2 = findAllNumbersDisappearedinAnArray(nums);
+		for (Integer integer : list2) {
+			System.out.println(integer);
+		}
+	}
+}
